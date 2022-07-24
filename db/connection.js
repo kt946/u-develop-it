15bbfc69995a97b03ab407342c68222573c750ef
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-07-24 19:04:48 -0400

    update connection.js

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
         // Your MySQL username,
         user: 'root',
         // Your MySQL password
-        password: 'pen963DraGon!',
+        password: 'IamRoot!',
         database: 'election'
     },
     console.log('Connected to the election database.')
